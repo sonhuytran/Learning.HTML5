58256753d3e9e4605ce4f1a9a1b5aa120bc9b24a
Son-Huy TRAN <sonhuytran@gmail.com>
2015-01-20 18:00:56 +0100

    Graphics - Colors - Patterns

--- Learning.HTML5/Canvas/basic-drawings/basic-drawings.js
+++ Learning.HTML5/Canvas/basic-drawings/basic-drawings.js
@@ -2,18 +2,37 @@
  * Created by stran on 19/01/2015.
  */
 
-// Constants
+// Constants....................................................................
 
+/**
+ * Number of canvas
+ * @type {number}
+ */
 var N_CANVAS = 4;
 
 // Global Variables.............................................................
 
-var canvas = [];
-var contexts = [];
-var drawCanvas = [];
+/**
+ * Array of all canvas
+ * @type {Array}
+ */
+var canvas = [],
+    /**
+     * Array of all graphics contexts
+     * @type {Array}
+     */
+    contexts = [],
+    /**
+     * Array of all drawing functions
+     * @type {Array}
+     */
+    drawCanvas = [];
 
 // Functions....................................................................
 
+/**
+ * Load all the canvas & their contexts to the array
+ */
 function loadCanvas() {
     canvas = [];
 
@@ -34,11 +53,16 @@ function loadCanvas() {
     console.log(contexts);
 }
 
+/**
+ * Search for the canvas HTML element from its id
+ * @param id
+ * @returns {HTMLElement}
+ */
 function getCanvas(id) {
     return document.getElementById('canvas' + id);
 }
 
-drawCanvas[0] = function() {
+drawCanvas[0] = function () {
     contexts[0].lineJoin = 'round';
     contexts[0].lineWidth = 10;
 
@@ -46,7 +70,7 @@ drawCanvas[0] = function() {
     contexts[0].textAlign = 'center';
     var drawn = true;
 
-    var draw = function() {
+    var draw = function () {
         contexts[0].fillText('Click anywhere to erase', canvas[0].width / 2, 28);
 
         contexts[0].strokeRect(20, 100, 120, 120);
@@ -55,14 +79,14 @@ drawCanvas[0] = function() {
         drawn = true;
     };
 
-    var clear = function() {
+    var clear = function () {
         contexts[0].fillText('Click anywhere to draw', canvas[0].width / 2, 28);
 
         drawn = false;
     };
 
     draw();
-    canvas[0].onmouseup = function() {
+    canvas[0].onmouseup = function () {
         contexts[0].clearRect(0, 0, canvas[0].width, canvas[0].height);
 
         if (drawn) {
@@ -73,7 +97,7 @@ drawCanvas[0] = function() {
     };
 };
 
-drawCanvas[1] = function() {
+drawCanvas[1] = function () {
     contexts[1].lineJoin = 'round';
     contexts[1].lineWidth = 10;
 
@@ -86,7 +110,7 @@ drawCanvas[1] = function() {
 
     var drawn = true;
 
-    var draw = function() {
+    var draw = function () {
         contexts[1].fillText('Click anywhere to erase', canvas[1].width / 2, canvas[1].height / 2);
 
         contexts[1].strokeRect(20, 100, 120, 120);
@@ -95,14 +119,14 @@ drawCanvas[1] = function() {
         drawn = true;
     };
 
-    var clear = function() {
+    var clear = function () {
         contexts[1].fillText('Click anywhere to draw', canvas[1].width / 2, canvas[1].height / 2);
 
         drawn = false;
     };
 
     draw();
-    canvas[1].onmouseup = function() {
+    canvas[1].onmouseup = function () {
         contexts[1].clearRect(0, 0, canvas[1].width, canvas[1].height);
 
         if (drawn) {
@@ -111,6 +135,37 @@ drawCanvas[1] = function() {
             draw();
         }
     };
+};
+
+drawCanvas[2] = function () {
+    var gradient = contexts[2].createLinearGradient(0, 0, canvas[2].width, 0);
+
+    gradient.addColorStop(0, 'blue');
+    gradient.addColorStop(0.25, 'white');
+    gradient.addColorStop(0.5, 'purple');
+    gradient.addColorStop(0.75, 'red');
+    gradient.addColorStop(1, 'yellow');
+
+    contexts[2].fillStyle = gradient;
+    contexts[2].rect(0, 0, canvas[2].width, canvas[2].height);
+    contexts[2].fill();
+};
+
+drawCanvas[3] = function () {
+    var gradient = contexts[3].createRadialGradient(
+        canvas[3].width / 2, canvas[3].height, 10,
+        canvas[3].width / 2, 0, 100);
+
+    gradient.addColorStop(0, 'blue');
+    gradient.addColorStop(0.25, 'white');
+    gradient.addColorStop(0.5, 'purple');
+    gradient.addColorStop(0.75, 'red');
+    gradient.addColorStop(1, 'yellow');
+
+    contexts[3].fillStyle = gradient;
+    contexts[3].rect(0, 0, canvas[3].width, canvas[3].height);
+    contexts[3].fill();
+
 };
 
 // Initialization
